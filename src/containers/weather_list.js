be4f0375d4dd006fdbbd7ac1fde9ff7277e22a6b
xiaojundi <dixiaojunfrank@gmail.com>
2018-04-28 13:50:36 -0400

    willing to add google map

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,19 @@
 import React, {Component} from "react"
 import {connect} from "react-redux"
 import TemGraph from '../components/tempgraph'
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends Component{
 	renderWeather(cityData){
 		const name = cityData.city.name
 		const temps = cityData.list.map(weather=>weather.main.temp)
+		const lon = cityData.city.coord.lon
+		const lat = cityData.city.coord.lat
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td>
+					<GoogleMap lon={lon} lat={lat} />
+				</td>
 				<td>
 					<TemGraph  data={temps}/>
 				</td>
